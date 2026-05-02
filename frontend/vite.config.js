88efa29318b60d71859f6747c34e2a71386b5a85
minpyojeong96-svg <6w2cjqyw5y@privaterelay.appleid.com>
2026-05-02 23:16:35 +0900

    fix: remove PWA/service worker (causing Forbidden error)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icons/*.png'],
-      manifest: false,
-      strategies: 'generateSW',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-            handler: 'CacheFirst',
-            options: { cacheName: 'google-fonts' }
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
